d1fe4efbc5e9b70012a64695db56593a66008efe
Ansarissab <zahidensari116@gmail.com>
2023-12-06 02:47:02 +0500

    Added mysql database and migrations to create book table

---
+++ BookListRazor/Models/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Models
+{
+	public class ApplicationDBContext : DbContext 
+	{
+		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+		{
+
+		}
+			
+		public DbSet<Book> Book { get; set; }
+	}
+}
+
--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,7 @@ namespace BookListRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDBContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
